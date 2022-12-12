c22ddc61b31a12f822b8d5f85153771210b1b0b6
Agent-A451 <alphonsusoko9@gmail.com>
2022-12-12 14:17:51 -0800

    Almost

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int s;
+	char low;
+
+
+	for (s = '0'; s <= '9'; s++)
+		putchar(s);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
