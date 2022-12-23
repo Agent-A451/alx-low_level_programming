0bbdde36302ff9fa574c787f141a8d4bc841e52e
Agent-A451 <alphonsusoko9@gmail.com>
2022-12-22 19:27:46 -0800

    hot fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,19 +10,14 @@
 
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
+	int i;
+	int j;
 
-	a = [1, 2, 3, 4]
-	aux = 1;
-	a = [4, 3, 2, 1]
-	p = a;
 	for (i = 0; i < n; i++)
-		p++;
-	for (k = 0; k < n / 2; k++)
 	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+		n--;
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
 	}
 }
