cfc6682ed1d3d223da00130b75b7f25427806fcc
Agent-A451 <alphonsusoko9@gmail.com>
2022-12-22 20:18:23 -0800

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,27 @@
 #include "main.h"
 
 /**
- *
+ *cap_string - capitalization string
+ *@n: string param
+ *Return: capitalized version of string
+ */
+
+char *cap_string(char *n)
+{
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'}
+	int len = 13;
+	int a = 0, i;
+
+	while (n[a])
+	{
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] = s[a] - 32;
+			i++;
+		}
+		a++;
+	}
+	return (n);
+}
