e955fd46d84355bfc60e90463b2db497657be4b3
Agent-A451 <alphonsusoko9@gmail.com>
2022-12-22 23:01:01 -0800

    Advanced 4

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,47 +1,47 @@
 #include "main.h"
 
 /**
- * infinite_add - Adds the numbers stored in two strings.
- * @n1: The string containing the first number to be added.
- * @n2: The string containing the second number to be added.
- * @r: The buffer to store the result.
- * @size_r: The current size of the buffer.
- * Return: If r can store the sum - a pointer to the result.
- * If r cannot store the sum - 0.
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ * ahhh! Crazy task! Expand your knowledge
+ * Return: address of r or 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int num, tens = 0;
+	int i, j, k, l, m, n;
 
-	for (; *n1 && *n2; n1--, n2--, size_r--)
-	{
-		num = (*n1 - '0') + (*n2 - '0');
-		num += tens;
-		*(r + size_r) = (num % 10) + '0';
-		tens = num / 10;
-	}
-
-	for (; *n1; n1--, size_r--)
-	{
-		num = (*n1 - '0') + tens;
-		*(r + size_r) = (num % 10) + '0';
-		tens = num / 10;
-	}
-
-	for (; *n2; n2--, size_r--)
-	{
-		num = (*n2 - '0') + tens;
-		*(r + size_r) = (num % 10) + '0';
-		tens = num / 10;
-	}
-
-	if (tens && size_r >= 0)
-	{
-		*(r + size_r) = (tens % 10) + '0';
-		return (r + size_r);
-	}
-
-	else if (tens && size_r < 0)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
 		return (0);
-	return (r + size_r + 1);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
